580860cb90aa856dcf09718a2234b374710e8cb3
juniorbrew-dotcom <juniorbrew@gmail.com>
2025-08-30 19:32:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("rj-cache").then(cache => {
+    caches.open('v1').then(cache => {
       return cache.addAll([
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
