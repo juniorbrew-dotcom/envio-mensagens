69a117717d0c01a9a6344a559e23cc20029f3fa4
juniorbrew-dotcom <juniorbrew@gmail.com>
2025-08-30 19:46:12 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,37 @@
+const CACHE_NAME = 'cliente-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instala o service worker e armazena os arquivos no cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Ativa o service worker e remove caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercepta requisições e responde com cache quando possível
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
